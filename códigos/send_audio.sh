51084adafc8efa3e9009dd51ba62b508631429c5
Ana Barbosa <84107511+Anak-Barbosa@users.noreply.github.com>
2025-07-08 16:50:01 -0400

    Delete códigos/send_audio.sh

--- códigos/send_audio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# IP do Raspberry Pi (altere conforme necessário)
-RASPBERRY_IP="000.000.000.000"
-PORTA=1234
-
-ffmpeg -f alsa -i default -ac 1 -ar 16000 -acodec pcm_s16le -f wav tcp://$RASPBERRY_IP:$PORTA
